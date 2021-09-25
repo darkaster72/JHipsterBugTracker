c7cc6cec001f0876f6834790c9c0250d3598193c
Ankit Sharma <sankit72@gmail.com>
2021-09-25 11:02:12 +0530

    added missing files

--- webpack/webpack.custom.js
+++ webpack/webpack.custom.js
@@ -123,6 +123,7 @@ module.exports = async (config, options, targetOptions) => {
         groupBy: [
           { pattern: './src/main/webapp/i18n/en/*.json', fileName: './i18n/en.json' },
           { pattern: './src/main/webapp/i18n/hi/*.json', fileName: './i18n/hi.json' },
+          { pattern: './src/main/webapp/i18n/ja/*.json', fileName: './i18n/ja.json' },
           // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
         ],
       },
